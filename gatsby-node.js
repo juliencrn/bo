4c2abd25c13a95d491eca61ed93c0fbfde9a9278
Julien <juliencaron18@gmail.com>
2019-09-11 18:17:55 +0200

    Refactor gatsby-node.js part

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -11,41 +10,8 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 }
 
+const createPacks = require('./gatsby/createPacks')
+
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  // Destructure the createPage function from the actions object
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMdx {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-            fileAbsolutePath
-          }
-        }
-      }
-    }
-  `)
-  if (result.errors) {
-    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
-  }
-
-  // Filter posts only
-  const postsRegex = RegExp(/(blog)\/.*\.md$/)
-  const posts = result.data.allMdx.edges.filter(({ node }) =>
-    postsRegex.test(node.fileAbsolutePath)
-  )
-
-  // We'll call `createPage` for each result
-  posts.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/post.jsx`),
-      // We can use the values in this context in
-      context: { id: node.id, layout: 'post' }
-    })
-  })
+  await createPacks({ graphql, actions, reporter })
 }
---
+++ gatsby/createPacks.js
@@ -0,0 +1,43 @@
+const path = require(`path`)
+
+/**
+ * Create Packs
+ */
+module.exports = async ({ actions, graphql, reporter }) => {
+  // Destructure the createPage function from the actions object
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            fileAbsolutePath
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
+  }
+
+  // Filter posts only
+  const postsRegex = RegExp(/(blog)\/.*\.md$/)
+  const posts = result.data.allMdx.edges.filter(({ node }) =>
+    postsRegex.test(node.fileAbsolutePath)
+  )
+
+  // We'll call `createPage` for each result
+  posts.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/post.jsx`),
+      // We can use the values in this context in
+      context: { id: node.id, layout: 'post' }
+    })
+  })
+}
